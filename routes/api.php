fe89af9e5d20047f8b76dfe231ec574a9b515a75
xombol <cobl-m@mail.ru>
2022-11-11 16:09:48 +0200

    clean project

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\AuthorController;
+use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\API\PublisherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('book', BookController::class);
+Route::resource('author', AuthorController::class);
+Route::resource('publisher', PublisherController::class);
+
